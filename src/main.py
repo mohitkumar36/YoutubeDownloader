9900facd7aa67955ced80df4a3b43d706172b449
Mohit Kumar <57846872+mohitkumar36@users.noreply.github.com>
2023-08-17 04:52:51 +0530

    Update main.py
    Added the option from multiple download quality
    Added support for audio only download
    Added Download progress bar in the console
    Added Metadata of the video in the ui

--- src/main.py
+++ src/main.py
@@ -1,50 +1,142 @@
 from ast import Break
 from re import L
-from tkinter import *
-from pytube import YouTube
+import tkinter as tk
+import pytube
 from tkinter import messagebox, filedialog
+import ffmpeg
+import progressbar as progress
+from pytube.cli import on_progress
+import os
+import subprocess
+import threading
+
+class CmdThread (threading.Thread):
+   def __init__(self, command, textvar):
+        threading.Thread.__init__(self)
+        self.command = command
+        self.textvar = textvar
+
+   def run(self):
+        proc = subprocess.Popen(self.command, stdout=subprocess.PIPE)
+        while not proc.poll():
+            data = proc.stdout.readline()
+            if data:
+                print(data)
+                self.textvar.set(data)
+            else:
+                break
+
+def progress(streams, chunk: bytes, bytes_remaining: int):
+    contentsize = video.filesize
+    size = contentsize - bytes_remaining
+
+    print('\r' + '[Download progress]:[%s%s]%.2f%%;' % (
+    '█' * int(size*20/contentsize), ' '*(20-int(size*20/contentsize)), float(size/contentsize*100)), end='')
+   
+YT = ''
+video = ''
+
+def download():
+    global video
 
 
-def downloadHigh():
- 
-    # getting user-input Youtube Link
-    Youtube_link = url.get()
- 
-    #getting dwnld dir
+    RES = clicked.get()
+
     download_Folder = dest.get()
- 
-    # Creating object of YouTube()
-    getVideo = YouTube(Youtube_link)
- 
-    # Getting 720p streams of the video
-    videoStream = getVideo.streams.get_highest_resolution()
- 
-    # Downloading the video to dir
-    videoStream.download(download_Folder)
- 
-    # popup message
-    messagebox.showinfo("SUCCESSFULLY",
-                        "DOWNLOADED AND SAVED IN\n"
-                        + download_Folder)
 
-def downloadLow():
+    if RES == "audio":
+        video = YT.streams.filter(abr="160kbps", progressive=False).first()
+        video.download(download_Folder, filename=f"{YT.title}.mp3")
+        messagebox.showinfo("SUCCESSFULLY",
+                            "DOWNLOADED AND SAVED IN\n"
+                            + download_Folder)
+        
+    elif RES not in ['1080p', '1440p']:
+        video = YT.streams.filter(res=RES, progressive=True).first()
  
-    Youtube_link = url.get()
- 
-    download_Folder = dest.get()
- 
-    getVideo = YouTube(Youtube_link)
- 
-    videoStream = getVideo.streams.filter(res="360p").first()
- 
-    videoStream.download(download_Folder)
- 
-    messagebox.showinfo("SUCCESSFULLY",
-                        "DOWNLOADED AND SAVED IN\n"
-                        + download_Folder)
+        video.download(download_Folder)
+    
+        messagebox.showinfo("SUCCESSFULLY",
+                            "DOWNLOADED AND SAVED IN\n"
+                            + download_Folder)
+    else:
+        if download_Folder:
+            download_Folder += '/'
+
+        # download audio only
+        video = YT.streams.filter(abr="160kbps", progressive=False).first()
+        video.download(download_Folder, filename="audio.mp3")
+        print("\nAudio download success")
+
+        video = YT.streams.filter(res="1080p", progressive=False).first()
+        video.download(download_Folder, filename="video.mp4")
+        print("\nVideo download success")
+
+        audio = ffmpeg.input(f"{download_Folder}audio.mp3")
+
+        #download video only
+
+        video = ffmpeg.input(f"{download_Folder}video.mp4")
+
+        title = YT.title
+        title = ''.join(letter for letter in title if letter.isalnum())
+
+        ffmpeg.output(audio, video, f"{download_Folder}{title}.mp4").run(overwrite_output=True)
+
+        os.remove(f"{download_Folder}audio.mp3")
+        os.remove(f"{download_Folder}video.mp4")
+        messagebox.showinfo("SUCCESSFULLY",
+                            "DOWNLOADED AND SAVED IN\n"
+                            + download_Folder)
 
 
-def Browse():
+def videodetails():
+    link = url.get()
+    if not link:
+        messagebox.showinfo("ERROR",
+                            "Enter a link")
+        return
+    try:
+        yt = pytube.YouTube(link, on_progress_callback=progress)
+    except:
+        messagebox.showinfo("ERROR",
+                            "Enter a valid link")
+        return
+
+
+    title.config(text = f"Title: {yt.title}")
+    author.config(text = f"Author: {yt.author}")
+    publishDate.config(text = f"Published date: {yt.publish_date.strftime('%Y-%m-%d')}")
+    views.config(text = f"Number of views: {yt.views}")
+    duration.config(text=f"Length of video: {yt.length} seconds")
+
+    resolutions = set()
+    for e in yt.streams:
+        for quality in ['144p',"240p", "360p", "480p", "720p", "1080p", "1440p", "audio"]:
+            if quality in str(e):
+                resolutions.add(quality)
+
+    def show(x):
+        clicked.set(x)
+
+    resolutions = list(resolutions)
+
+    clicked.set('')
+    qualitySelect['menu'].delete(0, 'end')
+
+    # Insert list of new options (tk._setit hooks them up to var)
+    for choice in resolutions:
+        qualitySelect['menu'].add_command(label=choice, command=tk._setit(clicked, choice))
+    clicked.set(resolutions[0])
+
+    global YT
+    YT = yt
+    
+
+
+
+
+def browse():
     #getting directory
     download_Directory = filedialog.askdirectory(
         initialdir="YOUR DIRECTORY PATH", title="Save Video")
@@ -52,77 +144,114 @@ def Browse():
     #inserting dir in the textbox
     dest.insert(0, download_Directory) 
 
-if __name__ == "__main__":
-    root = Tk()
-    #root.iconbitmap('info.ico')
-    root.geometry("390x200")
-    root.resizable(False, False)
-    root.title("Youtube Downloader")
-    #root.config(background="#e87878")
 
-    #labels
-    label1 = Label(root, 
-                   text = "Youtube link : ",
-                   padx=10,
-                   pady=10)
+#MAIN
+root = tk.Tk()
+# root.iconbitmap('info.ico')
+root.geometry("")
+root.resizable(False, False)
+root.title("Youtube Downloader")
+root.eval('tk::PlaceWindow . center')
+#root.config(background="#e87878")
 
-    label2 = Label(root, 
-                   text="Destination : ", 
-                   padx=10,
-                   pady=5)
-    
-    #buttons
-    browse = Button(root,
-                    text="Browse",
-                    command=Browse,
-                    width=10)
-    downloadhigh = Button(root, 
-                          text = "Download 720p Quality", 
-                          command = downloadHigh)
-    downloadlow = Button(root, 
-                         text = "Download 360p Quality", 
-                         command = downloadLow)
+#labels
+label1 = tk.Label(root, 
+                text = "Youtube link: ",
+                padx=10,
+                pady=10)
 
-    #inputs
-    url = Entry(root, width=35)
-    dest = Entry(root)
+label2 = tk.Label(root, 
+                text="Destination: ", 
+                padx=10,
+                pady=5)
 
-    #ui
+title = tk.Label(root, text="Title:",padx=10,
+                pady=10)
+author = tk.Label(root, text="Author:",padx=10,
+                pady=10)
+publishDate = tk.Label(root, text="Published date:",padx=10,
+                pady=10)
+views = tk.Label(root, text="Number of views:",padx=10,
+                pady=10)
+duration = tk.Label(root, text="Length of video:",padx=10,
+                pady=10)
 
-    label1.grid(row=2,
-                 column=0,
-                 pady=5,
-                 padx=5)
 
-    url.grid(row=2,
-             column=1,
-             pady=5,
-             padx=5,
-             columnspan=2)
+text = tk.StringVar()
+downloadProgress = tk.Label(textvariable=text)
 
-    label2.grid(row=3,
+thread = CmdThread(['ipconfig'], text)
+
+#buttons
+browse = tk.Button(root,
+                text="Browse",
+                command=browse,
+                width=10)
+
+download = tk.Button(root, 
+                        text = "Download", 
+                        command = download)
+
+getDetails = tk.Button(root, text = "Get Details", command=videodetails)
+
+
+clicked = tk.StringVar()
+qualitySelect = tk.OptionMenu(root, clicked,'')
+# qualitySelect.pack()
+
+#inputs
+url = tk.Entry(root, width=35)
+dest = tk.Entry(root)
+
+#ui
+
+label1.grid(row=2,
                 column=0,
                 pady=5,
                 padx=5)
 
-    dest.grid(row=3,
-              column=1,
-              pady=5,
-              padx=5)
+url.grid(row=2,
+            column=1,
+            pady=5,
+            padx=5,
+            columnspan=2)
 
-    browse.grid(row=3,
-                column=2,
-                 pady=1,
-                 padx=1)
+getDetails.grid(row=2, column=3,
+                pady=20,
+                padx=20)
 
-    downloadhigh.grid(row=4,
-                  column=1,
-                  pady=20,
-                  padx=20)
+label2.grid(row=3,
+            column=0,
+            pady=5,
+            padx=5)
 
-    downloadlow.grid(row=5,
-                  column=1,
-                  pady=5,
-                  padx=20)
+dest.grid(row=3,
+            column=1,
+            pady=5,
+            padx=5)
 
-    root.mainloop()
+browse.grid(row=3,
+            column=2,
+                pady=1,
+                padx=1)
+
+qualitySelect.grid(row=4,
+                column=3)
+
+download.grid(row=5,
+                column=3)
+
+title.grid(row=4,column=1)
+author.grid(row=5,column=1)
+publishDate.grid(row=6,column=1)
+views.grid(row=7,column=1)
+duration.grid(row=8,column=1)
+# downloadProgress.grid(row=9, column=1, pady=5)
+messagebox.showinfo("INFO",
+                        "Console will show the download progress and errors.\n \nDownloading videos higher than 720p requires ffmpeg installed on you computer and download time will depend on computer hardware.")
+
+# qualitySelect.grid(row=)
+
+# thread.start()
+
+root.mainloop()
