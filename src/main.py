67832525f5e7fdd4d25b5f054cfe2559a4a2269f
Mohit Kumar <57846872+mohitkumar36@users.noreply.github.com>
2023-09-29 23:53:41 +0530

    Update main.py
    Added a thumbnail after getting video details.
    Added more audio choices and "2160p" video option.
    Now the UI will show download size while quality selection.
    Major bug fixed which wouldn't allow user to download video greater than "1080p" in resolutions selection.

--- src/main.py
+++ src/main.py
@@ -1,30 +1,21 @@
-from ast import Break
-from re import L
+import io
+from collections import defaultdict
 import tkinter as tk
+from tkinter.constants import NW
+
 import pytube
 from tkinter import messagebox, filedialog
 import ffmpeg
 import progressbar as progress
-from pytube.cli import on_progress
 import os
 import subprocess
-import threading
 
-class CmdThread (threading.Thread):
-   def __init__(self, command, textvar):
-        threading.Thread.__init__(self)
-        self.command = command
-        self.textvar = textvar
+import requests, shutil
+from PIL import ImageTk, Image
+from PIL.Image import Resampling
+from io import StringIO
 
-   def run(self):
-        proc = subprocess.Popen(self.command, stdout=subprocess.PIPE)
-        while not proc.poll():
-            data = proc.stdout.readline()
-            if data:
-                print(data)
-                self.textvar.set(data)
-            else:
-                break
+
 
 def progress(streams, chunk: bytes, bytes_remaining: int):
     contentsize = video.filesize
@@ -32,16 +23,21 @@ def progress(streams, chunk: bytes, bytes_remaining: int):
 
     print('\r' + '[Download progress]:[%s%s]%.2f%%;' % (
     '█' * int(size*20/contentsize), ' '*(20-int(size*20/contentsize)), float(size/contentsize*100)), end='')
-   
+
+
 YT = ''
 video = ''
+
 
 def download():
     global video
 
-
     RES = clicked.get()
-
+    for r in ['144p', "240p", "360p", "480p", "720p", "1080p", "1440p", "2160p", "128kbps", "160kbps"]:
+        if r in RES:
+            RES = r
+            break
+    # print(RES)
     if not url.get():
         messagebox.showinfo("Error",
                             "Enter a download link")
@@ -49,18 +45,18 @@ def download():
 
     download_Folder = dest.get()
 
-    if RES == "audio":
-        video = YT.streams.filter(abr="160kbps", progressive=False).first()
+    if RES in ["128kbps", "160kbps"]:
+        video = YT.streams.filter(abr=RES, progressive=False).first()
         video.download(download_Folder, filename=f"{YT.title}.mp3")
         messagebox.showinfo("SUCCESSFULLY",
                             "DOWNLOADED AND SAVED IN\n"
                             + download_Folder)
-        
-    elif RES not in ['1080p', '1440p']:
+
+    elif RES not in ['1080p', '1440p', "2160p"]:
         video = YT.streams.filter(res=RES, progressive=True).first()
- 
+
         video.download(download_Folder)
-    
+
         messagebox.showinfo("SUCCESSFULLY",
                             "DOWNLOADED AND SAVED IN\n"
                             + download_Folder)
@@ -76,7 +72,7 @@ def download():
         video.download(download_Folder, filename="audio.mp3")
         print("\nAudio download success")
 
-        video = YT.streams.filter(res="1080p", progressive=False).first()
+        video = YT.streams.filter(res=RES, progressive=False).first()
         video.download(download_Folder, filename="video.mp4")
         print("\nVideo download success")
 
@@ -87,29 +83,34 @@ def download():
                 en_caption = caption
         # print(en_caption.xml_captions)
 
-        if language: 
-        # Instead of Captions in XML format we are converting it to string format.
+        if language:
+            # Instead of Captions in XML format we are converting it to string format.
             en_caption_convert_to_srt = (en_caption.generate_srt_captions())
-            with open(download_Folder+'subs.srt', "w", encoding="utf-8") as f:
+            with open(download_Folder + 'subs.srt', "w", encoding="utf-8") as f:
                 f.write(en_caption_convert_to_srt)
 
         try:
-            #f'ffmpeg -i {download_Folder}video.mp4 -i {download_Folder}audio.mp3 -c:v copy -c:a copy {download_Folder+title}.mp4'
+            # f'ffmpeg -i {download_Folder}video.mp4 -i {download_Folder}audio.mp3 -c:v copy -c:a copy {
+            # download_Folder+title}.mp4'
             if language:
-                subprocess.run(['ffmpeg', '-i', download_Folder+'video.mp4', '-i',  download_Folder+"audio.mp3",'-i', download_Folder+'subs.srt' ,"-c:v", "copy", "-c:a", "copy", '-c:s', 'mov_text', download_Folder+title+'.mp4'], check=True)
+                subprocess.run(
+                    ['ffmpeg', '-i', download_Folder + 'video.mp4', '-i', download_Folder + "audio.mp3", '-i',
+                     download_Folder + 'subs.srt', "-c:v", "copy", "-c:a", "copy", '-c:s', 'mov_text',
+                     download_Folder + title + '.mp4'], check=True)
             else:
-                subprocess.run(['ffmpeg', '-i', download_Folder+'video.mp4', '-i', download_Folder + "audio.mp3", "-c:v", "copy", "-c:a", "copy", download_Folder+title+'.mp4'], check=True)
+                subprocess.run(
+                    ['ffmpeg', '-i', download_Folder + 'video.mp4', '-i', download_Folder + "audio.mp3", "-c:v", "copy",
+                     "-c:a", "copy", download_Folder + title + '.mp4'], check=True)
         except:
-            # print ('wrongcommand does not exist')
+            # print ('wrong-command does not exist')
             audio = ffmpeg.input(f"{download_Folder}audio.mp3")
 
-            #download video only
+            # download video only
 
             video = ffmpeg.input(f"{download_Folder}video.mp4")
 
-            
-
-            ffmpeg.output(audio, video, f"{download_Folder}{title}.mp4", vcodec='copy', acodec='copy').run(overwrite_output=True)
+            ffmpeg.output(audio, video, f"{download_Folder}{title}.mp4", vcodec='copy', acodec='copy').run(
+                overwrite_output=True)
 
         os.remove(f"{download_Folder}audio.mp3")
         os.remove(f"{download_Folder}video.mp4")
@@ -128,26 +129,51 @@ def videodetails():
         return
     try:
         yt = pytube.YouTube(link, on_progress_callback=progress)
+
     except:
         messagebox.showinfo("Error",
                             "Enter a valid link")
         return
 
+    global YT
+    YT = yt
 
-    title.config(text = f"Title: {yt.title}")
-    author.config(text = f"Author: {yt.author}")
-    publishDate.config(text = f"Published date: {yt.publish_date.strftime('%Y-%m-%d')}")
-    views.config(text = f"Number of views: {yt.views}")
+    title.config(text=f"Title: {yt.title}")
+    author.config(text=f"Author: {yt.author}")
+    publishDate.config(text=f"Published date: {yt.publish_date.strftime('%Y-%m-%d')}")
+    views.config(text=f"Number of views: {yt.views}")
     duration.config(text=f"Length of video: {yt.length} seconds")
 
-    resolutions = set()
+    # creating the resolutions list
+    resolutions = {}
+    resolutions = defaultdict(lambda: 0, resolutions)
     for e in yt.streams:
-        for quality in ['144p',"240p", "360p", "480p", "720p", "1080p", "1440p", "audio"]:
+        # print(str(e))
+        for quality in ['144p', "240p", "360p", "480p", "720p", "1080p", "1440p", "2160p", "128kbps", "160kbps"]:
+            # print(str(e))
             if quality in str(e):
-                resolutions.add(quality)
+                video_size = e.filesize_mb
 
+                # adding audio tag to the audio files
+                if quality in ["128kbps", "160kbps"]:
+                    quality = f'Audio ({quality})'
 
-    resolutions = sorted(list(resolutions), key= lambda x:(len(x), x))
+                # Ignoring the non-progressive videos
+                if quality in ['144p', "240p", "360p", "480p", "720p"]:
+                    if 'progressive="True"' not in str(e):
+                        continue
+
+                # adding audio file size to the total size
+                if quality in ["1080p", "1440p", "2160p"]:
+                    video_size += YT.streams.filter(abr="160kbps", progressive=False).first().filesize_mb
+
+                resolutions[quality] = max(resolutions[quality], round(video_size, 2))
+
+    resolutions = [(q, s) for q, s in sorted(resolutions.items(), key=lambda x: (len(x[0]), x[0]))]
+    new = []
+    for q, s in resolutions:
+        new.append(f'{q} ({s} MB)')
+    resolutions = new
 
     clicked.set('')
     qualitySelect['menu'].delete(0, 'end')
@@ -157,94 +183,103 @@ def videodetails():
         qualitySelect['menu'].add_command(label=choice, command=tk._setit(clicked, choice))
     clicked.set(resolutions[0])
 
-    global YT
-    YT = yt
-    
 
 
+    # getting thumnail
+    r = requests.get(yt.thumbnail_url, stream=True)
+    if r.status_code == 200:
+        with open("img.jpeg", 'wb') as f:
+            r.raw.decode_content = True
+            shutil.copyfileobj(r.raw, f)
+
+    image1 = Image.open("img.jpeg")
+    image1 = image1.resize((160, 90), Resampling.LANCZOS)
+    test = ImageTk.PhotoImage(image1)
+
+    label_thumbnail = tk.Label(image=test)
+    label_thumbnail.image = test
+
+    label_thumbnail.grid(row=5, column=0, rowspan=3, pady=10, padx=10)
+    os.remove('img.jpeg')
 
 
 def browse():
-    #getting directory
+    # getting directory
     download_Directory = filedialog.askdirectory(
         initialdir="YOUR DIRECTORY PATH", title="Save Video")
- 
-    #inserting dir in the textbox
-    dest.insert(0, download_Directory) 
+
+    # inserting dir in the textbox
+    dest.insert(0, download_Directory)
 
 
-#MAIN
+# MAIN
 root = tk.Tk()
 # root.iconbitmap('info.ico')
 root.geometry("")
 root.resizable(False, False)
 root.title("Youtube Downloader")
 root.eval('tk::PlaceWindow . center')
-#root.config(background="#e87878")
+# root.config(background="#e87878")
 
-root.option_add( "*font", "lucida 10" )
+root.option_add("*font", "lucida 10")
 
-#labels
-label1 = tk.Label(root, 
-                text = "Youtube link: ",
-                padx=10,
-                pady=10)
+# labels
+label1 = tk.Label(root,
+                  text="Youtube link: ",
+                  padx=10,
+                  pady=10)
 
-label2 = tk.Label(root, 
-                text="Destination: ", 
-                padx=10,
-                pady=5)
+label2 = tk.Label(root,
+                  text="Destination: ",
+                  padx=10,
+                  pady=5)
 
-title = tk.Label(root, text="Title:",padx=10,
-                pady=10)
-author = tk.Label(root, text="Author:",padx=10,
-                pady=10)
-publishDate = tk.Label(root, text="Published date:",padx=10,
-                pady=10)
-views = tk.Label(root, text="Number of views:",padx=10,
-                pady=10)
-duration = tk.Label(root, text="Length of video:",padx=10,
-                pady=10)
-
+title = tk.Label(root, text="Title:", padx=10,
+                 pady=10)
+author = tk.Label(root, text="Author:", padx=10,
+                  pady=10)
+publishDate = tk.Label(root, text="Published date:", padx=10,
+                       pady=10)
+views = tk.Label(root, text="Number of views:", padx=10,
+                 pady=10)
+duration = tk.Label(root, text="Length of video:", padx=10,
+                    pady=10)
 
 text = tk.StringVar()
 downloadProgress = tk.Label(textvariable=text)
 
-thread = CmdThread(['ipconfig'], text)
-
-#buttons
+# buttons
 browse = tk.Button(root,
-                text="Browse",
-                command=browse,
-                width=10)
+                   text="Browse",
+                   command=browse,
+                   width=10)
 
-download = tk.Button(root, 
-                        text = "Download", 
-                        command = download, bg='#567', fg='White')
+download = tk.Button(root,
+                     text="Download",
+                     command=download, bg='#567', fg='White')
 
-getDetails = tk.Button(root, text = "Get Details", command=videodetails)
-
+getDetails = tk.Button(root, text="Get Details", command=videodetails)
 
 clicked = tk.StringVar()
-qualitySelect = tk.OptionMenu(root, clicked,'')
+qualitySelect = tk.OptionMenu(root, clicked, '')
 # qualitySelect.pack()
 
-#inputs
+# inputs
 url = tk.Entry(root, width=35)
 dest = tk.Entry(root)
 
-#ui
+# ui
 
 label1.grid(row=2,
-                column=0,
-                pady=5,
-                padx=5)
+            column=0,
+            pady=5,
+            padx=5)
 
 url.grid(row=2,
-            column=1,
-            pady=5,
-            padx=5,
-            columnspan=2)
+         column=1,
+         pady=5,
+         padx=5,
+         columnspan=2)
 
 getDetails.grid(row=2, column=3,
                 pady=20,
@@ -256,29 +291,29 @@ label2.grid(row=3,
             padx=5)
 
 dest.grid(row=3,
-            column=1,
-            pady=5,
-            padx=5)
+          column=1,
+          pady=5,
+          padx=5)
 
 browse.grid(row=3,
             column=2,
-                pady=1,
-                padx=1)
+            pady=1,
+            padx=1)
 
 qualitySelect.grid(row=4,
-                column=3)
+                   column=3)
 
 download.grid(row=5,
-                column=3)
+              column=3)
 
-title.grid(row=4,column=1)
-author.grid(row=5,column=1)
-publishDate.grid(row=6,column=1)
-views.grid(row=7,column=1)
-duration.grid(row=8,column=1)
+title.grid(row=4, column=1)
+author.grid(row=5, column=1)
+publishDate.grid(row=6, column=1)
+views.grid(row=7, column=1)
+duration.grid(row=8, column=1)
 # downloadProgress.grid(row=9, column=1, pady=5)
 messagebox.showinfo("INFO",
-                        "Console will show the download progress and Errors.\n \nDownloading videos higher than 720p requires ffmpeg installed on you computer and download time will depend on computer hardware.")
+                    "Console will show the download progress and Errors.\n \nDownloading videos higher than 720p requires ffmpeg installed on you computer and download time will depend on computer hardware.")
 
 # qualitySelect.grid(row=)
 
